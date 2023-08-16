5de4c40ae36db90e0b7a5c5e31a6caa174705a99
Maksim Pukshlis <maksimpusklis7@gmail.com>
2023-08-16 03:08:35 +0300

    update check_data for excel

--- src/menu/worker/tasks/utils_for_sync_excel/parse_excel.py
+++ src/menu/worker/tasks/utils_for_sync_excel/parse_excel.py
@@ -11,19 +11,19 @@ def is_valid_data(model: str, data: list) -> bool:
     if model == 'menu':
         expected_types_menu: list[Any] = [UUID, str, str]
         for index, item in enumerate(data):
-            if item == '' and not isinstance(item, expected_types_menu[index]):
+            if item == '' or not isinstance(item, expected_types_menu[index]):
                 return False
         return True
     elif model == 'submenu':
         expected_types_submenu: list[Any] = [UUID, str, str, UUID]
         for index, item in enumerate(data):
-            if item == '' and not isinstance(item, expected_types_submenu[index]):
+            if item == '' or not isinstance(item, expected_types_submenu[index]):
                 return False
         return True
     elif model == 'dish':
         expected_types_dish: list[Any] = [UUID, str, str, Union[Decimal, int, float], UUID]
         for index, item in enumerate(data):
-            if item == '' and not isinstance(item, expected_types_dish[index]):
+            if item == '' or not isinstance(item, expected_types_dish[index]):
                 return False
         return True
     return False
@@ -48,8 +48,9 @@ def parse_workbook(sheet) -> tuple[list[MenuModel], list[SubmenuModel], list[Any
 
     for row in sheet.iter_rows(min_row=1, values_only=True):
         if row[0] is not None:
+            menu_id, submenu_id = None, None
             if is_valid_uuid(row[0]):
-                check_data = is_valid_data('menu', [row[0], row[1], row[2]])
+                check_data = is_valid_data('menu', [UUID(row[0]), row[1], row[2]])
                 if check_data:
                     menu_id = UUID(row[0])
                     menu_data.append(MenuModel(id=menu_id, title=row[1], description=row[2]))
@@ -57,9 +58,10 @@ def parse_workbook(sheet) -> tuple[list[MenuModel], list[SubmenuModel], list[Any
                 else:
                     continue
         if row[1] is not None:
+            submenu_id = None
             if is_valid_uuid(row[1]):
-                if menu_id != '':
-                    check_data = is_valid_data('submenu', [row[1], row[2], row[3], menu_id])
+                if menu_id is not None:
+                    check_data = is_valid_data('submenu', [UUID(row[1]), row[2], row[3], menu_id])
                     if check_data:
                         submenu_id = UUID(row[1])
                         submenu_data.append(
@@ -68,12 +70,11 @@ def parse_workbook(sheet) -> tuple[list[MenuModel], list[SubmenuModel], list[Any
                 continue
         if row[2] is not None:
             if is_valid_uuid(row[2]):
-                if submenu_id != '':
-                    check_data = is_valid_data('dish', [row[2], row[3], row[4], row[5], submenu_id])
+                if submenu_id is not None:
+                    check_data = is_valid_data('dish', [UUID(row[2]), row[3], row[4], row[5], submenu_id])
                     if check_data:
                         dish_data.append(
                             [DishModel(id=UUID(row[2]), title=row[3], description=row[4], price=row[5],
-                                       submenu_id=submenu_id),
-                             menu_id
-                             ])
+                                       submenu_id=submenu_id), menu_id])
+
     return menu_data, submenu_data, dish_data
